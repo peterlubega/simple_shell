424d060be670591b27b0857188108904c3b43460
peterlubega <*peterlubega100@gmail.com>
2023-11-10 05:38:44 -0800

    feat: Simple shell 0.2 and Simple shell 0.3

--- process.c
+++ process.c
@@ -3,6 +3,7 @@
 #include <sys/wait.h>
 #include "error.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * createChildProcess - Creates a child process using fork().
@@ -19,14 +20,61 @@ return (fork());
  * @command: The command to execute.
  * @args: An array of arguments for the command.
  */
+
 void executeCommand(const char *command, char * const args[])
 {
-if (execve(command, args, NULL) == -1)
+/* Get the PATH environment variable */
+char *path = getenv("PATH");
+
+if (path == NULL)
+{
+/* Handle the case where PATH is not set */
+reportError("getenv(PATH)");
+exit(1);
+}
+
+/* Duplicate path string to avoid modifying the original */
+char *pathCopy = strdup(path);
+
+if (pathCopy == NULL)
+{
+reportError("strdup");
+exit(1);
+}
+
+/* Declare variables */
+
+char *dir = strtok(pathCopy, ":");
+/* Declare variables before using them */
+while (dir != NULL)
+{
+/* Construct the full path to the command */
+char fullPath[1024];
+/* Assuming a maximum path length of 1024 */
+snprintf(fullPath, sizeof(fullPath), "%s/%s", dir, command);
+
+/* Check if the command exists in the current directory */
+if (access(fullPath, X_OK) == 0)
+{
+/* Command exists, execute it */
+if (execve(fullPath, args, NULL) == -1)
 {
 reportError("execve");
 exit(1);
 }
 }
+
+/* Move to the next directory in PATH */
+dir = strtok(NULL, ":");
+}
+
+
+/* If the loop completes, the command was not found in any directory */
+fprintf(stderr, "Command not found: %s\n", command);
+exit(1);
+}
+
+
 
 /**
  * waitForChildProcess - Waits for the child process to finish.
