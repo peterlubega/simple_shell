0d9ac8af0835c61fa55a931ec75e3f4c80aaff3b
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-15 01:09:39 -0800

    refact: remove practice process files

--- process.c
+++ process.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "error.h"
 #include <stdlib.h>
+#include <string.h>
+#include "error.h"
+#include "process.h"
+
+extern char **environ;
 
 /**
  * createChildProcess - Creates a child process using fork().
@@ -15,25 +19,100 @@ return (fork());
 }
 
 /**
- * executeCommand - Executes a command with arguments in the child process.
- * @command: The command to execute.
- * @args: An array of arguments for the command.
- */
-void executeCommand(const char *command, char * const args[])
-{
-if (execve(command, args, NULL) == -1)
-{
-reportError("execve");
-exit(1);
-}
-}
-
-/**
  * waitForChildProcess - Waits for the child process to finish.
  * @childPid: The PID of the child process to wait for.
  */
 void waitForChildProcess(pid_t childPid __attribute__((unused)))
 {
 int status;
-wait(&status);
+waitpid(childPid, &status, 0);
+}
+
+/**
+ * executeCommand - Executes a command with arguments in the child process.
+ * @command: The command to execute.
+ * @args: An array of arguments for the command.
+ */
+void executeCommand(const char *command, char *const args[])
+{
+/* Check if the command is an absolute path */
+if (strchr(command, '/') != NULL)
+{
+pid_t child_pid = fork();
+
+if (child_pid == -1)
+{
+perror("fork");
+exit(1);
+}
+else if (child_pid == 0)
+{
+/* In the child process */
+printf("Executing: %s\n", command);
+execve(command, args, NULL);
+/* If execve fails, report an error */
+perror("execve");
+_exit(1);
+}
+else
+{
+/* In the parent process */
+waitpid(child_pid, NULL, 0);
+}
+}
+else
+
+
+{
+/* Find the command in the PATH */
+char *path = getenv("PATH");
+char *token;
+char commandPath[1024];  /* Assuming a maximum path length */
+int found = 0;
+
+token = strtok(path, ":");
+
+while (token != NULL)
+{
+strcpy(commandPath, token);
+strcat(commandPath, "/");
+strcat(commandPath, command);
+
+if (access(commandPath, X_OK) == 0)
+{
+/* If the command exists, execute it */
+pid_t child_pid = createChildProcess();
+
+if (child_pid == -1)
+{
+perror("fork");
+exit(1);
+}
+else if (child_pid == 0)
+{
+/* In the child process */
+printf("Executing: %s\n", commandPath);
+execve(commandPath, args, environ);
+/* If execve fails, report an error */
+perror("execve");
+exit(1);
+}
+else
+{
+/* In the parent process */
+waitpid(child_pid, NULL, 0);
+found = 1;
+break;  /* Break after waiting for the child process */
+}
+}
+
+token = strtok(NULL, ":");
+}
+if (!found)
+{
+/* If the loop completes, the command was not found */
+fprintf(stderr, "Command not found: %s\n", command);
+exit(1);
+}
+}
 }
