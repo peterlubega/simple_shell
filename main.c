db45e3ec3804a982c32080e4afa92c69670f330c
peterlubega <*peterlubega100@gmail.com>
2023-11-15 02:15:39 -0800

    refact: fixed betty issues in other files except process

--- main.c
+++ main.c
@@ -5,8 +5,58 @@
 #include "error.h"
 #include <string.h>
 
-
 #define MAX_COMMAND_LENGTH 1024
+
+/**
+ * handleExitCommand - Handles the exit command.
+ *
+ * This function is called when the user inputs the "exit" command. It prints
+ * a message indicating the shell is exiting and terminates the program.
+ */
+void handleExitCommand(void)
+{
+printf("Exiting the shell...\n");
+exit(0);
+}
+
+/**
+ * executeChildProcess - Executes a child process.
+ *
+ * This function creates a child process, handles errors if the fork fails,
+ * and executes the command with its arguments in the child process. It waits
+ * for the child process to complete in the parent process.
+ *
+ * @input: The user input containing the command and arguments.
+ */
+void executeChildProcess(char *input)
+{
+pid_t child_pid = createChildProcess();
+
+if (child_pid == -1)
+{
+reportError("fork");
+exit(1);
+}
+else if (child_pid == 0)
+{
+char *args[MAX_COMMAND_LENGTH];
+int i = 0;
+args[i] = strtok(input, " ");
+
+while (args[i] != NULL)
+{
+i++;
+args[i] = strtok(NULL, " ");
+}
+
+executeCommand(args[0], args);
+exit(1);
+}
+else
+{
+waitForChildProcess(child_pid);
+}
+}
 
 /**
  * main - Entry point for the simple shell program.
@@ -20,7 +70,6 @@
 int main(void)
 {
 char input[MAX_COMMAND_LENGTH];
-pid_t child_pid;
 
 while (1)
 {
@@ -30,41 +79,12 @@ fflush(stdout);
 readUserInput(input, MAX_COMMAND_LENGTH);
 cleanInput(input);
 
-/* Check for the exit command */
 if (strcmp(input, "exit") == 0)
 {
-printf("Exiting the shell...\n");
-exit(0);
+handleExitCommand();
 }
 
-child_pid = createChildProcess();
-
-if (child_pid == -1)
-{
-reportError("fork");
-exit(1);
-}
-else if (child_pid == 0)
-{
-/* Split the input into command and arguments */
-char *args[MAX_COMMAND_LENGTH];
-int i = 0;
-args[i] = strtok(input, " ");
-
-while (args[i] != NULL)
-{
-i++;
-args[i] = strtok(NULL, " ");
-}
-
-/* Call executeCommand with the command and arguments */
-executeCommand(args[0], args);
-exit(1);
-}
-else
-{
-waitForChildProcess(child_pid);
-}
+executeChildProcess(input);
 }
 
 return (0);
